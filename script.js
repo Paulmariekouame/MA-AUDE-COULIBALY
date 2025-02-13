6a6c04c94ca6e289bc838bd1de33762ecbbec22f
Paulmariekouame <paulkouame973@gmail.com>
2025-02-13 13:34:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function nextPage() {
+    window.location.href = 'index3.html'; // Redirige vers la prochaine page
+}
+
+function moveButton() {
+    const button = document.getElementById('noButton');
+    button.style.position = 'absolute';
+    button.style.top = Math.random() * window.innerHeight + 'px';
+    button.style.left = Math.random() * window.innerWidth + 'px';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const noButton = document.getElementById('noButton');
+    noButton.addEventListener('mouseover', moveButton);
+});
+
